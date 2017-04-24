b9311969319ebc67ecfcd39a92acefa834705121
Simon Scarfe <simon.scarfe@gmail.com>
2017-04-25 00:43:33 +0100

    initial commit

---
+++ pinboard.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Pinboard Aggregator
+Plugin URI: https://github.com/simons/pinboard-aggregator
+Description: Posts aggregated link lists from Pinboard.in to your WordPress blog
+Version: 1.0
+Author: Simon Scarfe
+Author URI: https://breakfastdinnertea.co.uk
+License: GPL2
+*/
