56ec93278c2167cde1280e849bd33b92e73a913d
Simon Scarfe <simon.scarfe@gmail.com>
2017-04-25 01:54:30 +0100

    get pinboard posts from last week

--- pinboard.php
+++ pinboard.php
@@ -8,3 +8,22 @@ Author: Simon Scarfe
 Author URI: https://breakfastdinnertea.co.uk
 License: GPL2
 */
+
+// insert auth token here, this will eventually be pulled from settings
+$url = "https://api.pinboard.in/v1/posts/recent?auth_token=&tag=";
+
+function getPosts($url) {
+    $pinboard_request = wp_remote_retrieve_body(wp_remote_get($url));
+    $pinboard_xml = simplexml_load_string($pinboard_request, 'SimpleXMLElement', LIBXML_NOCDATA);
+
+    return $pinboard_xml->xpath('//post');
+}
+
+function isInLastWeek($post) {
+    $weekAgo = date_create('-7 days');
+    $postTime = date_create($post['time']);
+
+    return $postTime > $weekAgo;
+}
+
+$pinboard_posts = array_filter(getPosts($url), "isInLastWeek");
